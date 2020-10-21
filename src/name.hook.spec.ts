d6401d8b02c4c35ccbfde3d319d4becbdf4f35f3
Medrano <medrana2@emea.roche.com>
2020-10-21 15:55:41 +0200

    repair

--- src/name.hook.spec.ts
+++ src/name.hook.spec.ts
@@ -23,7 +23,7 @@ describe('useName specs', () => {
         result.current.setName(newName);
         });
         // Assert 
-        expect(result.current.name).toEqual('name-FAIL-y mas')
+        expect(result.current.name).toEqual('name')
     });
 
   })
